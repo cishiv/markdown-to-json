b79ca19d43abddb1d79d11ef2a02231341a47f35
Shivan <shivantheman@gmail.com>
2022-04-16 23:27:55 +0200

    add start line matchers for ul

--- markdown/markdown.go
+++ markdown/markdown.go
@@ -24,13 +24,15 @@ type Block struct {
 // paragraph is fallback
 var blockpatterns = map[string]string{
 	// These matchers _should_ work because we're matching blocks line-by-line
+	// recurrence capture doesn't bubble up the way I expect, this needs to be handled programatically
+	// if something is an ###, it can't be ## or #
 	"heading1":   "^#{1}",
 	"heading2":   "^#{2}",
 	"heading3":   "^#{3}",
 	"linebreak":  "\n",
-	"blockquote": "^>",       // support only single level blocks (blocks can contain other elements).
-	"codeblock":  "^```",     // this one needs depth.
-	"ul":         `\-|\*|\+`, // this one is debatable.
+	"blockquote": "^>",          // support only single level blocks (blocks can contain other elements).
+	"codeblock":  "^```",        // this one needs depth.
+	"ul":         `^\-|^\*|^\+`, // this one is debatable.
 	"ol":         `^[0-9]*\.`,
 	"hr":         "^---",
 }
