3941243db3dfab1ca2bf11a087341387ea1d30b9
Shivan <shivantheman@gmail.com>
2022-04-19 13:11:49 +0200

    markdown: add test for end to end

--- markdown/markdown_test.go
+++ markdown/markdown_test.go
@@ -1,23 +1,46 @@
 package markdown
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"markdown-to-json/utils"
 	"os"
 	"testing"
 )
 
-func TestPatterns(t *testing.T) {
+func TestEndToEndParser(t *testing.T) {
 	dat, err := os.ReadFile("./examples/small-test.md")
 	if err != nil {
 		panic(err)
 	}
 	result := fileToSlice(string(dat))
 	patterns := compilePatterns()
-	// o(n^2) nice :)
 	matchMap := match(patterns, result)
 	postprocessedlines := precompute(matchMap)
 	serializedLines := out(postprocessedlines)
 	jsonString := utils.MapToJsonString(serializedLines)
-	_ = ioutil.WriteFile("examples/test-result.json", []byte(jsonString), 0644)
+
+	expected, err := os.Open("./examples/expected-result.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	byteResult, _ := ioutil.ReadAll(expected)
+	var res map[int]Line
+	json.Unmarshal([]byte(byteResult), &res)
+
+	eq := true
+
+	// TODO: Needs a better assertion.
+	if len(res[0].Results) != len(serializedLines[0].Results) {
+		eq = false
+	}
+
+	if !eq {
+		t.Error("Result was not as expected")
+		_ = ioutil.WriteFile("examples/test-result-fail.json", []byte(jsonString), 0644)
+	} else {
+		_ = ioutil.WriteFile("examples/test-result.json", []byte(jsonString), 0644)
+	}
 }
